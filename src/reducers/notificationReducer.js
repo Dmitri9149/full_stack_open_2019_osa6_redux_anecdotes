1018ff64126eb3b2acaacb1d5cccd53f5b5c1da1
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-06-17 18:15:35 +0300

    initial commit

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,28 @@
+
+
+
+const initialState = 'Initial notification'
+
+const notificationReducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.notification
+    default:
+      return state
+  }
+}
+
+
+export const setNotification = (notification) => {
+  return {
+    type: 'SET_NOTIFICATION',
+    notification
+  }
+}
+
+
+
+export default notificationReducer
