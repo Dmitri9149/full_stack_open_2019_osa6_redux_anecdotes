d0680d09e7b366161256108ed934ed3a704eb370
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-06-17 10:02:02 +0300

    initial commit

---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { vote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = ({store}) => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => 
+              store.dispatch(vote(anecdote.id))
+            }
+            >
+            vote 
+            </button>
+          </div>
+        </div>
+      )}
+
+    </div>
+  )
+}
+
+export default  AnecdoteList 
